aa3c866076be35761ef480626ac19dc451e6de87
Janak <janak.agarwal@gmail.com>
2013-07-10 23:04:27 +0000

    update to read from index

--- web.js
+++ web.js
@@ -5,17 +5,17 @@ var app = express.createServer(express.logger());
 
 var content = new Buffer(256);
 
-fs.readFileSync('./index.html', function read(err, data) {
-   if (err) {
-      throw err;
-   }
-   content = data; //.write(data, "utf-8)";  
-});
+//fs.readFileSync('./index.html', function read(err, data) {
+//   if (err) {
+//      throw err;
+//   }
+//   content = data; //.write(data, "utf-8)";  
+//});
 
 //var text = content.toString('utf-8');
 
 app.get('/', function(request, response) {
-  response.send(content.toString('utf-8'));
+  response.send(content.toString('utf-8', fs.readFileSync("index.html")));
 });
 
 var port = process.env.PORT || 5000;
